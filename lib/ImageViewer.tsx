fc8eeb5c905d719dfa12ab84bdbd3a7de450fd8f
KKosana318 <kkosana33@gmail.com>
2023-07-23 16:42:20 -0400

    add swipe down to close

--- lib/ImageViewer.tsx
+++ lib/ImageViewer.tsx
@@ -148,6 +148,8 @@ export default function ImageViewer({
 		.onEnd((event) => {
 			if (scale.value === 1) {
                 const topBoundary = -finalHeight - dimensions.height;
+                const bottomBoundary = dimensions.height + finalHeight;
+
 				if (event.translationY < -50) {
 					if (event.velocityY < -2000 || event.translationY < -200) {
                         translateY.value = withTiming(topBoundary, { duration: 200 }, () => {
@@ -157,6 +159,15 @@ export default function ImageViewer({
 						return;
 					}
 				}
+
+                if (event.translationY > 50) {
+                    if (event.velocityY > 2000 || event.translationY > 200) {
+                        translateY.value = withTiming(bottomBoundary, { duration: 200 }, () => {
+                            runOnJS(onRequestClose)();
+                        });
+                        return;
+                    }
+                }
 
 				translateY.value = withTiming(0);
 				translateX.value = withTiming(0);
